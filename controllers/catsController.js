78b16dade3c445687e4147ab982d2a65b9b0b714
kbefff <kbeffel10@me.com>
2019-03-30 10:56:15 -0700

    server connection

--- controllers/catsController.js
+++ controllers/catsController.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/cat.js");
+var cat = require("../models/index.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
