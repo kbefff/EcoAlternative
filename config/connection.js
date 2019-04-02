5da7c4bcd0067408f07e65fe8ff9e9912a85ac16
Andrew Rice <andrewrice@Andrews-MacBook.hsd1.wa.comcast.net>
2019-04-01 22:17:55 -0700

    Merged password and account creation into full app

--- config/connection.js
+++ config/connection.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "password",
+  password: "di77on&&",
   database: "eco_alternative"
 });
 
