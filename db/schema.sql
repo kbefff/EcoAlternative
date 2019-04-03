d722fd72dde3b2e3b86626eae8d2f73659777bd6
kbefff <kbeffel10@me.com>
2019-04-03 16:18:49 -0700

    schema and seed data converted to sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,17 +2,39 @@ DROP DATABASE IF EXISTS eco_alternative;
 CREATE DATABASE eco_alternative;
 USE eco_alternative;
 
-
-CREATE TABLE categories(
-    categoryId INTEGER  NOT NULL PRIMARY KEY, 
-    category   VARCHAR(26) NOT NULL
+CREATE TABLE category(
+   categoryId    INTEGER  NOT NULL PRIMARY KEY 
+  ,category_name VARCHAR(24)
 );
 
+
 CREATE TABLE product(
-    categoryId INTEGER,
-    productId INTEGER,
-    product   VARCHAR(33),
-    PRIMARY KEY (categoryId, productId)
+   categoryId        INTEGER  NOT NULL
+  ,product_generalId INTEGER  NOT NULL
+  ,product_general   VARCHAR(22) NOT NULL
+id PRIMARY KEY (categoryId, product_generalId)
+
+);
+
+CREATE TABLE productOption(
+   categoryId           INTEGER  NOT NULL
+  ,category_name        VARCHAR(24) NOT NULL
+  ,product_generalId    INTEGER  NOT NULL
+  ,product_general      VARCHAR(18) NOT NULL
+  ,product_specificId   INTEGER  NOT NULL
+  ,product_specific     VARCHAR(56) NOT NULL
+  ,ingredients          VARCHAR(5) NOT NULL
+  ,plastic_free         VARCHAR(5) NOT NULL
+  ,reusable             VARCHAR(5) NOT NULL
+  ,price                VARCHAR(5) NOT NULL
+  ,stylish              VARCHAR(5) NOT NULL
+  ,environmental_impact VARCHAR(688) NOT NULL
+  ,image                VARCHAR(211) NOT NULL
+  ,external_link        VARCHAR(287) NOT NULL
+  ,fun_fact             VARCHAR(678) NOT NULL
+  ,source               VARCHAR(274) NOT NULL
+    id PRIMARY KEY (categoryId, product_generalId , product_specificId)
+
 );
 
 -- CREATE TABLE productOption(
@@ -22,7 +44,6 @@ CREATE TABLE product(
 
 
 
---     PRIMARY KEY (categoryId, productId, productOptionId)
 -- );
 
 
