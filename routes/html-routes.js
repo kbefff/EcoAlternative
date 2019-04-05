ec9ca5e44126a684d11451a779125b7b07aa4828
kbefff <kbeffel10@me.com>
2019-04-04 17:17:55 -0700

    orm routes and view bare bones

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,6 +4,10 @@ var path = require("path");
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
 var category = require("../models/category.js");
+var product = require("../models/product.js");
+var connection = require("../config/connection.js");
+
+
 
 module.exports = function (app) {
 
@@ -12,8 +16,7 @@ module.exports = function (app) {
     if (req.user) {
       res.render("index");
     }
-    // res.render("index");
-    // ==================== FROM CONTROLLER FOR NOW ==================
+
     // get all values in the category table
     category.all(function (data) {
       var hbsObject = {
@@ -22,11 +25,50 @@ module.exports = function (app) {
       };
       res.render("index", hbsObject);
     });
-    // ================================================================
 
     // res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
 
+  // go to specific clicked on product
+  app.get("/product", function (req, res) {
+    // // If the user already has an account send them to the members page
+    // if (req.user) {
+    //   res.render("index");
+    // }
+
+    // get all values in the product table
+    product.all(function (data) {
+      var hbsObject = {
+        products: data
+      };
+      res.render("product", hbsObject);
+    });
+
+
+  });
+
+  // when user clicks on any element from product table, trigger ajax to get /product/:id then we will see productOptions.all
+  // show the products on the product page
+  app.get("/product/:id", function (req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.render("index");
+    }
+
+    // TODO: refactor for orm
+    connection.query("SELECT * FROM productOption WHERE id = ?", [req.params.id], function (err, data) {
+      if (err) return res.status(500).end();
+      var hbsObject = {
+        products: data
+      };
+      res.render("productOption", hbsObject);
+    });
+  });
+
+
+
+
+  
   app.get("/login", function (req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
