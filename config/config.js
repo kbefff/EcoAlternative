efe80d51bd70986f418ad54b1c1a0356b46f97eb
tammyleeseattle <tammylee84@gmail.com>
2019-04-06 10:22:49 -0700

    adding index, categories and about us layouts

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
 
   "development": {
     "username": "root",
-    "password": "password",
+    "password": process.env.DB_PW,
     "database": "users",
     "host": "127.0.0.1",
     "dialect": "mysql"
