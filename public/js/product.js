953bab22f3550f917abf37289a6551d4cb057057
kbefff <kbeffel10@me.com>
2019-04-08 13:18:25 -0700

    add partial

---
+++ public/js/product.js
@@ -0,0 +1,12 @@
+
+//   document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('select');
+//     var instances = M.FormSelect.init(elems, options);
+//   });
+
+//   // Or with jQuery
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
+      
