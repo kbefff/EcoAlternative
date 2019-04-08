91f576be82e69227b72280d2169a2478e2f8c805
Andrew Rice <andrewrice@Andrews-MacBook.hsd1.wa.comcast.net>
2019-04-07 22:05:17 -0700

    Form submission for new products and also made adjustment to the schema so the primary key is being auto incremented.

--- db/schema.sql
+++ db/schema.sql
@@ -9,33 +9,50 @@ DROP TABLE IF EXISTS productOption;
 
 
 CREATE TABLE category(
-   id            INTEGER  NOT NULL PRIMARY KEY AUTO_INCREMENT
-  ,category_name VARCHAR(24) NOT NULL
-  ,icon VARCHAR(24) NOT NULL
+   id INTEGER  NOT NULL PRIMARY KEY AUTO_INCREMENT
+  ,category_name VARCHAR
+(24) NOT NULL
+  ,icon VARCHAR
+(24) NOT NULL
 );
 
 
 CREATE TABLE product(
-   id           INTEGER  NOT NULL PRIMARY KEY AUTO_INCREMENT
-  ,product_name VARCHAR(18) NOT NULL
+   id INTEGER  NOT NULL PRIMARY KEY AUTO_INCREMENT
+  ,product_name VARCHAR
+(18) NOT NULL
   ,categoryI_id INTEGER  NOT NULL
 );
 
-CREATE TABLE productOption(
-   id                   INTEGER  NOT NULL PRIMARY KEY 
-  ,product_name         VARCHAR(18) NOT NULL
-  ,productOption_name   VARCHAR(56) NOT NULL
+CREATE TABLE productOption
+(
+  id INTEGER NOT NULL
+  AUTO_INCREMENT PRIMARY KEY 
+  ,product_name         VARCHAR
+  (18) NOT NULL
+  ,productOption_name   VARCHAR
+  (56) NOT NULL
   ,product_Id           INTEGER  NOT NULL
-  ,ingredients          VARCHAR(5) NOT NULL
-  ,plastic_free         VARCHAR(5) NOT NULL
-  ,reusable             VARCHAR(5) NOT NULL
-  ,price                VARCHAR(5) NOT NULL
-  ,stylish              VARCHAR(5) NOT NULL
-  ,environmental_impact VARCHAR(688) NOT NULL
-  ,image                VARCHAR(211) NOT NULL
-  ,external_link        VARCHAR(287) NOT NULL
-  ,fun_fact             VARCHAR(678) NOT NULL
-  ,source               VARCHAR(274) NOT NULL
+  ,ingredients          VARCHAR
+  (5) NOT NULL
+  ,plastic_free         VARCHAR
+  (5) NOT NULL
+  ,reusable             VARCHAR
+  (5) NOT NULL
+  ,price                VARCHAR
+  (5) NOT NULL
+  ,stylish              VARCHAR
+  (5) NOT NULL
+  ,environmental_impact VARCHAR
+  (688) NOT NULL
+  ,image                VARCHAR
+  (211) NOT NULL
+  ,external_link        VARCHAR
+  (287) NOT NULL
+  ,fun_fact             VARCHAR
+  (678) NOT NULL
+  ,source               VARCHAR
+  (274) NOT NULL
 );
 
 
--- public/js/form.js
+++ public/js/form.js
@@ -2,17 +2,17 @@ $(document).ready(function () {
     $('select').formSelect();
 
 
-    //When variables captured the data is transformed into a json object upon capture
+    // When variables captured the data is transformed into a json object upon capture
     $(`#submitButton`).on(`click`, function (event) {
         event.preventDefault();
-        var userInput = {
-            product_specific: $(`#product_specific`).val(),
+        var newProduct = {
+            productOption_name: $(`#productOption_name`).val(),
             image: $(`#image`).val().trim(),
             external_link: $(`#external_link`).val().trim(),
             source: $(`#source`).val().trim(),
             price: $(`#price`).val().trim(),
-            category_name: $(`#category_name option:selected`).val(),
-            product_general: $(`#product_general option:selected`).val(),
+            product_Id: $(`#product_Id option:selected`).val(),
+            product_name: $(`#product_name option:selected`).text(),
             ingredients: $(`#ingredients option:selected`).text(),
             plastic_free: $(`#plastic_free option:selected`).text(),
             reusable: $(`#reusable option:selected`).text(),
@@ -20,51 +20,16 @@ $(document).ready(function () {
             fun_fact: $(`#fun_fact`).val(),
             environmental_impact: $(`#environmental_impact`).val()
         };
+        console.log(newProduct);
+        $.ajax("api/newproduct", {
+            type: "POST",
+            data: newProduct
+        }).then(
+            function () {
+                console.log("New Product Added!!!");
+                location.reload();
+            }
 
-
-
-
-        console.log(userInput);
-
-        logProduct(userInput.product_specific, userInput.image, userInput.external_link, userInput.source, userInput.price, userInput.category_name, userInput.product_general, userInput.ingredients, userInput.plastic_free, userInput.reusable, userInput.stylish, userInput.fun_fact, userInput.environmental_impact);
-        product_specific.val(``);
-        image.val(``);
-        external_link.val(``);
-        source.val(``);
-        price.val(``);
-        category_name.val(``);
-        product_general.val(``);
-        ingredients.val(``);
-        plastic_free.val(``);
-        reusable.val(``);
-        stylish.val(``);
-        fun_fact.val(``);
-        environmental_impact.val(``)
-
+        )
     });
-
-    function logProduct(product_specific) {
-        $.post("/api/newproduct", {
-            product_specific: product_specific,
-            image: image,
-            external_link: external_link,
-            source: source,
-            price: price,
-            category_name: category_name,
-            product_general: product_general,
-            ingredients: ingredients,
-            plastic_free: plastic_free,
-            reusable: reusable,
-            stylish: stylish,
-            fun_fact: fun_fact,
-            environmental_impact: environmental_impact
-
-        }).then(function (data) {
-            window.location.replace(data);
-            // If there's an error, log the error
-        }).catch(function (err) {
-            console.log(err);
-        });
-    }
-
 });
