83fc707fd1e38a570fe36f2a4592dfca1a705737
Andrew Rice <andrewrice@Andrews-MacBook.hsd1.wa.comcast.net>
2019-04-11 16:35:10 -0700

    Added environmental password variables to make it dynamic between local consoles

--- config/config.js
+++ config/config.js
@@ -5,7 +5,7 @@ module.exports = {
 
   "development": {
     "username": "root",
-    "password": "",
+    "password": process.env.PWD_DB,
     "database": "users",
     "host": "127.0.0.1",
     "dialect": "mysql"
